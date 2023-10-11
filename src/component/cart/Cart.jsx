69102882a14b61b478da9e094b24ef2ce671996f
seonekr <zaemuevln42gmail.com>
2023-10-11 16:03:26 +0700

    edit: payment page

--- src/component/cart/Cart.jsx
+++ src/component/cart/Cart.jsx
@@ -1,36 +1,15 @@
 import { FaArrowLeft } from "react-icons/fa6";
-import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
-import './cart.css'
-import Menu from '../menu/Menu';
-import { useState } from "react";
+import Menu from "../menu/Menu";
 import { Link } from "react-router-dom";
+import './cart.css';
 
 const Cart = () => {
-
-    const [quantity, setQuantity] = useState(1)
-
-    const handleInput = (e) => {
-        const value = parseInt(e.target.value);
-        if (!isNaN(value)) {
-            setQuantity(value);
-        }else{
-            setQuantity();
-        }
-    }
-
-    const handleAdd = () =>{
-        setQuantity(quantity + 1);
-    }
-
-    const handleDrop = () =>{
-        setQuantity(quantity - 1);
-    }
 
   return (
     <>
         <section id="header-account">
             <div className="account-navbar">
-                <div className="header-box"><Link to="/taca-app"><FaArrowLeft/></Link></div>
+                <div className="header-box"><Link to="/humascot-taca"><FaArrowLeft/></Link></div>
                 <div className="header-box middle">Cart</div>
                 <div className="header-box"></div>
             </div>
@@ -38,7 +17,7 @@ const Cart = () => {
         <section id="cart">
             <div className="cart-items">
                 <h1>Cart</h1>
-                <Link to="/taca-app/payment/">pay</Link>
+                <Link to="/humascot-taca/payment/">pay</Link>
             </div>
         </section>
         <Menu/>
