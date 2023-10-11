071bf840eda32643260527d4cd27bc074e9c0db6
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2023-10-11 17:04:02 +0700

    update page

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <Route path='/humascot-taca/chat' element={<Chat/>}/>
           <Route path='/humascot-taca' element={<Login/>}/>
           <Route path='/humascot-taca/menu' element={<Menu/>}/>
-          <Route path='/humascot-taca/components/login_register/Register' element={<Register/>}/>
+          <Route path='/humascot-taca/register' element={<Register/>}/>
           <Route path='/humascot-taca/cart' element={<Cart/>}/>
           <Route path='/humascot-taca/order' element={<Order/>}/>
           <Route path='/humascot-taca/product_search' element={<Product_search/>}/>
