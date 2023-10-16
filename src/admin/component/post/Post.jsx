05b6e57ed023ce23cff248b1e30e945a449bb6e4
seonekr <zaemuevln42gmail.com>
2023-10-16 09:13:50 +0700

    admin post

--- src/admin/component/post/Post.jsx
+++ src/admin/component/post/Post.jsx
@@ -4,19 +4,66 @@ import './post.css'
 const Post = () => {
     const [image, setImage] = useState([])
     const [images, setImages] = useState([]);
+    const [productName, setProductName] = useState('');
+    const [productType, setProductType] = useState('');
+    const [price, setPrice] = useState('');
+    const [details, setDetails] = useState('');
+
+
+    // Hanle submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('Form Data:', { // Here you can insert informatio to database
+            "Product name": productName,
+            "Product type": productType,
+            "Product price": price,
+            "Product details": details,
+            "Image": image,
+            "Gallery": images
+        });
+
+        setProductName('');
+        setProductType('');
+        setPrice('');
+        setDetails('')
+        setImage([]);
+        setImages([]);
+    };
+
+    // handle Product name
+    const handleProductName = (e) => {
+        const value = e.target.value
+        setProductName(value)
+    };
+    // handle Product type
+    const handleProductType = (e) => {
+        const value = e.target.value
+        setProductType(value)
+    };
+    // handle Product price
+    const handleProductPrice = (e) => {
+        const value = e.target.value
+        setPrice(value)
+    };
+    // handle Product details
+    const handleProductDetails = (e) => {
+        const value = e.target.value
+        setDetails(value)
+    };
 
     // image handle
     const handleImage = (e) => {
-        
+
         const file = e.target.files[0];
 
         if (file) {
           const reader = new FileReader();
-      
+
           reader.onloadend = () => {
             setImage([file]);
           };
-      
+
           reader.readAsDataURL(file);
         }
     };
@@ -31,23 +78,41 @@ const Post = () => {
         <>
             <AdminMenu/>
             <section id="post">
-                <form className="post-form">
+                <form onSubmit={handleSubmit} className="post-form">
                     <div className="input-box">
                         <div className="box">
                             <label htmlFor="productName">Product name</label>
-                            <input type="text" id="productName" placeholder="name"/>
+                            <input
+                                type="text"
+                                id="productName"
+                                placeholder="name"
+                                value={productName}
+                                onChange={handleProductName}
+                            />
                         </div>
                         <div className="box">
                             <label htmlFor="productType">Product type</label>
-                            <input type="text" id="productType" placeholder="type"/>
+                            <input
+                                type="text"
+                                id="productType"
+                                placeholder="type"
+                                value={productType}
+                                onChange={handleProductType}
+                            />
                         </div>
                         <div className="box">
                             <label htmlFor="price">Price</label>
-                            <input type="text" id="price" placeholder="price"/>
+                            <input
+                                type="text"
+                                id="price"
+                                placeholder="price"
+                                value={price}
+                                onChange={handleProductPrice}
+                            />
                         </div>
                         <div className="box">
                             <label htmlFor="details">Details</label>
-                            <textarea id="details" rows="10"></textarea>
+                            <textarea id="details" rows="10" value={details} onChange={handleProductDetails}></textarea>
                         </div>
                     </div>
                     <div className="input-img">
