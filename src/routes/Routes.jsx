9cf0c39d0e38d4e229e72d48256de19e7bc19e2e
seonekr <oudonephengkhamlar@gmail.com>
2023-10-17 15:23:12 +0700

    update routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -12,6 +12,22 @@ import OrderPage from "../admin/components/orderPage/OrderPage"
 import MenagerAdmin from "../admin/components/menagerAdmin/MenagerAdmin"
 import OrderPaid from "../admin/components/orderPage/OrderPaid";
 
+import Home from "../user/components/homepage/Home";
+import Account from "../user/components/account/Account";
+import General from "../user/components/account/General";
+import Contact from "../user/components/account/Contact";
+import Password from "../user/components/account/Password";
+import Payment from "../user/components/cart/Payment";
+import Cart from "../user/components/cart/Cart";
+import Address from "../user/components/cart/Address";
+import Chatuser from "../user/components/chat/Chatuser";
+import Chatroom from "../user/components/chat/Chatroom";
+import Order from "../user/components/order/Order";
+import Bill from "../user/components/order/Bill";
+import Dashboard from "../admin/Dashboard";
+import Post from "../admin/components/post/Post";
+import AddAdmin from "../admin/components/addAdmin/AddAdmin";
+
 const Links = () => {
     return(
         <Router>
@@ -27,6 +43,25 @@ const Links = () => {
                 <Route  exact  path='/humascot-taca/orderPage' Component={OrderPage}/>
                 <Route  exact  path='/humascot-taca/orderPaid' Component={OrderPaid}/>
                 <Route  exact  path='/humascot-taca/menagerAdmin' Component={MenagerAdmin}/>
+
+                {/* Sam */}
+                <Route exact path="/humascot-taca" Component={Home}/>
+                <Route exact path="/humascot-taca/account/" Component={Account}/>
+                <Route exact path="/humascot-taca/account/general/" Component={General}/>
+                <Route exact path="/humascot-taca/account/contact/" Component={Contact}/>
+                <Route exact path="/humascot-taca/account/password/" Component={Password}/>
+                <Route exact path="/humascot-taca/payment/" Component={Payment}/>
+                <Route exact path="/humascot-taca/cart/" Component={Cart}/>
+                <Route exact path="/humascot-taca/cart/address/" Component={Address}/>
+                <Route exact path="/humascot-taca/chatuser/" Component={Chatuser}/>
+                <Route exact path="/humascot-taca/chatuser/chatroom/" Component={Chatroom}/>
+                <Route exact path="/humascot-taca/order" Component={Order}/>
+                <Route exact path="/humascot-taca/order/bill" Component={Bill}/>
+
+                {/* Admin routes */}
+                <Route exact path="/humascot-taca/admin" Component={Dashboard}/>
+                <Route exact path="/humascot-taca/admin/post" Component={Post}/>
+                <Route exact path="/humascot-taca/admin/addadmin" Component={AddAdmin}/>
             </Routes>
       </Router>
     )
