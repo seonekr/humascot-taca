4316b81c158cafc3f9d85641fac108726660a2d2
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2023-10-17 16:31:38 +0700

    Update

--- src/user/components/chat/Chat.jsx
+++ src/user/components/chat/Chat.jsx
@@ -32,13 +32,13 @@ const Chat = () => {
                 </div>
             </div>
             <div className='container_chat_message'>
-                <div className='container_chat_text'>
+                <Link to="#" className='container_chat_text'>
                     <img src='https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Photos.png' alt='#'></img>
                     <div className='container_chat_name'>
                         <h4>Sompong</h4>
                         <p>New message...</p>
                     </div>
-                </div>
+                </Link>
                 <div className='container_chat_icon'>
                     {/* <from className="container_chat_icon_delete">
                         <section className='function_delete'>
