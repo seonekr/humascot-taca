bbf54a01399887052c6fc4db59371c1e39ea7152
seonekr <zaemuevln42gmail.com>
2023-10-24 16:31:31 +0700

    Price filter

--- src/user/components/products/ProductHome.jsx
+++ src/user/components/products/ProductHome.jsx
@@ -6,13 +6,16 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 const ProductHome = () => {
     const [products, setProducts] = useState([
-        { id: 1, name: 'Product 1', description: 'This is product 1', price: 10, images: [dress] },
-        { id: 2, name: 'Product 2', description: 'This is product 2', price: 20, images: [dress] },
-        { id: 3, name: 'Product 3', description: 'This is product 3', price: 30, images: [dress] },
-        { id: 4, name: 'Product 1', description: 'This is product 1', price: 10, images: [dress] },
-        { id: 5, name: 'Product 2', description: 'This is product 2', price: 20, images: [dress] },
-        { id: 6, name: 'Product 3', description: 'This is product 3', price: 30, images: [dress] },
+        { id: 1, name: 'Product 1', description: 'This is product 1', price: 12, category: "clothes", images: [dress] },
+        { id: 2, name: 'Product 2', description: 'This is product 2', price: 20, category: "clothes", images: [dress] },
+        { id: 3, name: 'Product 3', description: 'This is product 3', price: 30, category: "electronich device", images: [dress] },
+        { id: 4, name: 'Product 1', description: 'This is product 1', price: 10, category: "electronich device", images: [dress] },
+        { id: 5, name: 'Product 2', description: 'This is product 2', price: 20, category: "cosmetics", images: [dress] },
+        { id: 6, name: 'Product 3', description: 'This is product 3', price: 30, category: "cosmetics", images: [dress] },
       ]);
+
+    const [price, setPrice] = useState("");
+    const [priceFilter, setPriceFilter] = useState("");
 
     // Handle inputChange
     const handleInputChange = (e, index, field) => {
@@ -21,21 +24,39 @@ const ProductHome = () => {
         setProducts(updatedProducts);
     }
 
+    // Filter products based on search term and price range
+    const filteredProducts = products.filter((product) => {
+        const peiceMatch = priceFilter !== "" ? product.price === parseInt(priceFilter) : true;
+        return peiceMatch;
+    });
+
+    // Handle filter by category
+    const handleFilter = (price) => {
+        setPriceFilter(price); // Please change this to category
+    };
+
+    // Handle select by category
+    const handleSelectChange = (e) => {
+        setPrice(e.target.value); // Please change this to category
+        handleFilter(e.target.value); // Please change this to category
+    };
+
     return (
         <section id="product">
             <div className="productHead_content">
                 <h1 className="htxthead"><span className="spennofStyle"></span>Product</h1>
                 <form>
-                    <select className="filter_priceProduct">
-                        <option value="0">Price low to high</option>
-                        <option value="1">Price low</option>
-                        <option value="2">Price high</option>
+                    <select className="filter_priceProduct" value={price} onChange={handleSelectChange}>
+                        <option value="">All</option>
+                        <option value="10">Price low to high</option>
+                        <option value="20">Price low</option>
+                        <option value="30">Price high</option>
                     </select>
                 </form>
             </div>
 
             <form className="product-area">
-                {products.map((product, index) => (
+                {filteredProducts.map((product, index) => (
                     <div className="box-product" key={index}>
                         <Link to="#"><img src={product.images[0]} alt="image" /></Link>
                         <ul className="txtOFproduct">
