4ba5cef871a9964463bccb31a70d0453741a02e5
seonekr <145427113+seonekr@users.noreply.github.com>
2023-10-30 16:38:38 +0700

    update

--- src/user/components/account/Account.jsx
+++ src/user/components/account/Account.jsx
@@ -1,20 +1,22 @@
 import "./account.css"
-import { FaArrowLeft, FaAngleRight } from "react-icons/fa6";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import Header from "../header/Header";
 import { BiLogOut } from "react-icons/bi";
 import user from "../../../img/user.png";
 import Menu from "../menu/Menu"
 import { Link } from "react-router-dom";
 const Account = () => {
-    return(
+    return (
         <>
-            <section id="header-account">
-                <div className="account-navbar">
-                    <div className="header-box"><Link to="/"><FaArrowLeft/></Link></div>
-                    <div className="header-box middle">Account</div>
+            <Header />
+
+            <section id="account" className="accountContainer">
+                <div className="account_navbarr">
+                    <div className="header_boxBack"><Link to="/" className='guopIconbAck'><FaAngleLeft className='iconnBack' />Back</Link></div>
+                    <div className="headerTiele">Account</div>
                     <div className="header-box"></div>
                 </div>
-            </section>
-            <section id="account">
+
                 <div className="personal-info">
                     <div className="profile">
                         <div className="box-image"><span><img src={user} alt="" /></span></div>
@@ -22,25 +24,26 @@ const Account = () => {
                     </div>
                     <div className="text-info">
                         <Link to="/account/general">
-                            <span>General</span><FaAngleRight/>
+                            <span>General</span><FaAngleRight />
                         </Link>
                         <Link to="/account/contact">
-                            <span>Contact</span><FaAngleRight/>
+                            <span>Contact</span><FaAngleRight />
                         </Link>
                         <Link to="/account/password">
-                            <span>Password</span><FaAngleRight/>
+                            <span>Password</span><FaAngleRight />
                         </Link>
                     </div>
+
                     <div className="about-account">
                         <Link to="/" className="logout">
-                            <div className="icon-logout"><BiLogOut/></div>
+                            <div className="icon-logout"><BiLogOut /></div>
                             <div className="text-logout">Logout</div>
                         </Link>
                         <Link to="/">Delete account</Link>
                     </div>
                 </div>
             </section>
-            <Menu/>
+            <Menu />
         </>
     )
 };
