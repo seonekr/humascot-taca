496a067eb68fd0bd7455e035d0f0924f82395f85
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2023-11-08 15:56:26 +0700

    Update

--- src/admin/components/menagerUser/MenagerUser.jsx
+++ src/admin/components/menagerUser/MenagerUser.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import "./menagerUser.css"
 import { AiOutlineDelete,AiOutlineLeft,AiOutlineRight } from 'react-icons/ai';
 import { IoSearchOutline } from 'react-icons/io5';
-import { Link } from 'react-router-dom';
 import AdminMenu from '../adminMenu/AdminMenu';
 import users from '../../../img/users.png'
+import { useState } from 'react';
 
 const MenagerUser = () => {
-    
+    const [user, setUser] = useState([
+        { userID: 1, userName:"Sompong", message:"New Message...",images: [users] },
+        { userID: 2, userName:"Anoupha", message:"New Message...",images: [users] },
+        { userID: 3, userName:"Sengphachan",message:"New Message...", images: [users] },
+        { userID: 4, userName:"Khammun", message:"New Message...",images: [users] },
+        { userID: 5, userName:"Sompong", message:"New Message...",images: [users] },
+        { userID: 6, userName:"Sompong", message:"New Message...",images: [users] },
+    ]);
+
+   
+    // Delete
+    const handleDelete = (userID) => {
+      const updatedUser = user.filter((users) => users.userID !== userID);
+      setUser(updatedUser);
+    };
+
   return (
     <>
         <section id='menager'>
@@ -18,28 +33,36 @@ const MenagerUser = () => {
                         <h2>Users</h2>
                         <form className="search">
                             <div className="search-box_menageruser">
-                                <input type="text" placeholder="Search ..." />
+                                <input 
+                                    type="text" 
+                                    placeholder="Search ..." 
+                                   
+                                />
                                 <button type="submit">
                                 <IoSearchOutline />
                                 </button>
                             </div>
                         </form>
                     </div>
-                    <div className='box_users_user'>
-                        <div className='box_user_text'>
-                            <img src={users} alt='img'></img>
-                            <div className='container_chat_name'>
-                                <h4>Sompong</h4>
-                                <p>New message...</p>
-                            </div>
+                    {user.map((users) => (
+                        <div key={users.userName}>
+                            <form className='box_users_user'>
+                                <div className='box_user_text'>
+                                    <img src={users.images} alt='image'></img>
+                                    <div className='container_chat_name'>
+                                        <h4>{users.userName}</h4>
+                                        <p>{users.message}</p>
+                                    </div>
+                                </div>
+                                <div className='container_user_icon'>
+                                    <button className='btn_delete_user' onClick={() => handleDelete(users.userID)}>
+                                        <AiOutlineDelete id='icon_edit_delete'/>
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-                        <div className='container_user_icon'>
-                            <button className='btn_delete_user'>
-                                <AiOutlineDelete id='icon_edit_delete'/>
-                            </button>
-                        </div>
-                    </div>
-                    
+                    ))}
+
                     <div className='box_next_user'>
                         <button className='box_prev_next_user'>
                             <AiOutlineLeft id="box_prev_next_icon"/>
@@ -55,7 +78,8 @@ const MenagerUser = () => {
                             <AiOutlineRight id="box_prev_next_icon"/>
                         </button>
                     </div>
-                </div>
+                      
+                </div>   
             </div>
         </section>
     </>
