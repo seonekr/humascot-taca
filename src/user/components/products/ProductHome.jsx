5fcb3986684d9b84def322a14ab00fafab7b7b63
seonekr <zaemuevln42gmail.com>
2023-11-10 17:28:54 +0700

    fect data json file

--- src/user/components/products/ProductHome.jsx
+++ src/user/components/products/ProductHome.jsx
@@ -1,68 +1,66 @@
 import "./productHome.css";
-import dress from "../../../img/dress.png";
-import image1 from "../../../img/image1.png";
-import acer from '../../../img/acer.png';
-import productImage from "../../../img/productImage.png";
-import { useState } from "react";
+import { useState, useEffect  } from "react";
 import { useNavigate } from 'react-router-dom';
 
 
 const ProductHome = () => {
-    const [products, setProducts] = useState([
-        { productID: 1, productName: "pro1", productType: "clothes", price: 10, description: "desc for this product", images: [acer] },
-        { productID: 2, productName: "pro2", productType: "clothes", price: 10, description: "desc for this product", images: [dress] },
-        { productID: 3, productName: "pro3", productType: "clothes", price: 10, description: "desc for this product", images: [acer] },
-        { productID: 4, productName: "pro4", productType: "clothes", price: 10, description: "desc for this product", images: [dress] },
-        { productID: 5, productName: "pro5", productType: "clothes", price: 10, description: "desc for this product", images: [image1] },
-        { productID: 6, productName: "pro6", productType: "clothes", price: 10, description: "desc for this product", images: [image1] },
-        { productID: 7, productName: "pro7", productType: "clothes", price: 10, description: "desc for this product", images: [productImage] },
-        { productID: 8, productName: "pro8", productType: "clothes", price: 10, description: "desc for this product", images: [acer] },
-        { productID: 9, productName: "pro9", productType: "clothes", price: 10, description: "desc for this product", images: [productImage] },
-        { productID: 10, productName: "pro10", productType: "clothes", price: 10, description: "desc for this product", images: [acer] },
-        { productID: 11, productName: "pro11", productType: "clothes", price: 10, description: "desc for this product", images: [productImage] }
-    ]);
-
+    const [products, setProducts] = useState(null);
     const [price, setPrice] = useState("");
     const [priceFilter, setPriceFilter] = useState("");
     const [displayCount, setDisplayCount] = useState(12);
 
-    // Handle inputChange
-    const handleInputChange = (e, index, field) => {
-        const updatedProducts = [...products];
-        updatedProducts[index][field] = e.target.value;
-        setProducts(updatedProducts);
-    }
 
-    // Filter products based on search term and price range
-    const filteredProducts = products.filter((product) => {
-        const peiceMatch = priceFilter !== "" ? product.price === parseInt(priceFilter) : true;
-        return peiceMatch;
-    });
+    // Fetch data
+    useEffect(() => {
+      fetch('http://localhost:3000/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(error => console.log(error));
+    }, []);
 
-    // Handle filter by price
-    const handleFilter = (price) => {
-        setPriceFilter(price);
-    };
 
-    // Handle select by price
-    const handleSelectChange = (e) => {
-        setPrice(e.target.value);
-        handleFilter(e.target.value);
-    };
+     // Handle inputChange
+  const handleInputChange = (e, index, field) => {
+    const updatedProducts = [...products];
+    updatedProducts[index][field] = e.target.value;
+    setProducts(updatedProducts);
+  };
 
-    // Read more
-    const displayedProducts = filteredProducts.slice(0, displayCount);
-    const handleViewMore = () => {
-        setDisplayCount(displayCount + 4);
-    };
+  // Filter products based on search term and price range
+  const filteredProducts = products?.filter(product => {
+    const priceMatch = priceFilter !== "" ? product.price === parseInt(priceFilter) : true;
+    return priceMatch;
+  });
 
-    // Get send ID
-    const navigate = useNavigate();
+  // Handle filter by price
+  const handleFilter = price => {
+    setPriceFilter(price);
+  };
 
-    // Handle product
-    const handleProduct = (sendProductID) => {
-        navigate('/product_search/productdetails/', { state: { sendProductID: sendProductID } });
-    }
+  // Handle select by price
+  const handleSelectChange = e => {
+    setPrice(e.target.value);
+    handleFilter(e.target.value);
+  };
+
+  // Handle search term
+  const handleSearchTermChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Read more
+  const displayedProducts = filteredProducts?.slice(0, displayCount);
+  const handleViewMore = () => {
+    setDisplayCount(displayCount + 4);
+  };
+
+  // Get send ID
+  const navigate = useNavigate();
+
+  // Handle product
+  const handleProduct = sendProductID => {
+    navigate('/product_search/productdetails/', { state: { sendProductID: sendProductID } });
+  };
 
     return (
         <section id="product">
@@ -82,11 +80,11 @@ const ProductHome = () => {
             </div>
 
             <div className="product-area">
-                {displayedProducts.map((product, index) => (
+                {displayedProducts && displayedProducts.map((product, index) => (
                     <div className="box-product" key={index}>
                         <div onClick={() => handleProduct(product.productID)}>
                             <div className='img'>
-                                <img src={product.images[0]} alt="image" />
+                                <img src={product.images} alt={product.productName} />
                             </div>
                             <ul className="txtOFproduct2">
                                 <li>
