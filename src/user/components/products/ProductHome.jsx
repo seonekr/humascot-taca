1b80f7b622b6c449709adfd11db184fe15fe50a2
seonekr <zaemuevln42gmail.com>
2023-11-14 14:13:27 +0700

    filter products

--- src/user/components/products/ProductHome.jsx
+++ src/user/components/products/ProductHome.jsx
@@ -8,112 +8,117 @@ import { useNavigate } from 'react-router-dom';
 
 
 const ProductHome = () => {
-    const [products, setProducts] = useState([
-        {
-          productID: 1,
-          productName: "pro1",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [acer],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 2,
-          productName: "pro2",
-          productType: "clothes",
-          
-          price: 10,
-          description: "desc for this product",
-          images: [dress],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 3,
-          productName: "pro3",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [acer],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 4,
-          productName: "pro4",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [dress],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 5,
-          productName: "pro5",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [image1],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 6,
-          productName: "pro6",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [image1],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 7,
-          productName: "pro7",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [productImage],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 8,
-          productName: "pro8",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [acer],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 9,
-          productName: "pro9",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [productImage],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 10,
-          productName: "pro10",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [acer],
-          colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
-        },
-        {
-          productID: 11,
-          productName: "pro11",
-          productType: "clothes",
-          price: 10,
-          description: "desc for this product",
-          images: [productImage],
-          colors: [{colorID: 1, colorName: "red"}, {colorID: 2, colorName: "green"}]
-        },
-      ]);
+    // const [selectedPriceRange, setSelectedPriceRange] = useState('');
 
-    const [price, setPrice] = useState("");
-    const [priceFilter, setPriceFilter] = useState("");
-    const [displayCount, setDisplayCount] = useState(8);
+    // const [products, setProducts] = useState();
+      
+
+      const [selectedPriceRange, setSelectedPriceRange] = useState('');
+      const [originalProducts, setOriginalProducts] = useState(
+        [
+            {
+              productID: 1,
+              productName: "pro1",
+              productType: "clothes",
+              price: 10,
+              description: "desc for this product",
+              images: [acer],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 2,
+              productName: "pro2",
+              productType: "clothes",
+              price: 15,
+              description: "desc for this product",
+              images: [dress],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 3,
+              productName: "pro3",
+              productType: "clothes",
+              price: 12,
+              description: "desc for this product",
+              images: [acer],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 4,
+              productName: "pro4",
+              productType: "clothes",
+              price: 21,
+              description: "desc for this product",
+              images: [dress],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 5,
+              productName: "pro5",
+              productType: "clothes",
+              price: 22,
+              description: "desc for this product",
+              images: [image1],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 6,
+              productName: "pro6",
+              productType: "clothes",
+              price: 26,
+              description: "desc for this product",
+              images: [image1],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 7,
+              productName: "pro7",
+              productType: "clothes",
+              price: 30,
+              description: "desc for this product",
+              images: [productImage],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 8,
+              productName: "pro8",
+              productType: "clothes",
+              price: 34,
+              description: "desc for this product",
+              images: [acer],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 9,
+              productName: "pro9",
+              productType: "clothes",
+              price: 31,
+              description: "desc for this product",
+              images: [productImage],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 10,
+              productName: "pro10",
+              productType: "clothes",
+              price: 50,
+              description: "desc for this product",
+              images: [acer],
+              colors: [{colorID: 1, colorName: "black"}, {colorID: 2, colorName: "blue"}, {colorID: 3, colorName: "red"}, {colorID: 4, colorName: "green"}]
+            },
+            {
+              productID: 11,
+              productName: "pro11",
+              productType: "clothes",
+              price: 60,
+              description: "desc for this product",
+              images: [productImage],
+              colors: [{colorID: 1, colorName: "red"}, {colorID: 2, colorName: "green"}]
+            },
+          ]
+      );
+      const [products, setProducts] = useState(originalProducts);
+      const [displayCount, setDisplayCount] = useState(8);
 
     // Handle inputChange
     const handleInputChange = (e, index, field) => {
@@ -123,24 +128,49 @@ const ProductHome = () => {
     }
 
     // Filter products based on search term and price range
-    const filteredProducts = products.filter((product) => {
-        const peiceMatch = priceFilter !== "" ? product.price === parseInt(priceFilter) : true;
-        return peiceMatch;
-    });
+    // const filteredProducts = products.filter((product) => {
+    //     const peiceMatch = priceFilter !== "" ? product.price === parseInt(priceFilter) : true;
+    //     return peiceMatch;
+    // });
 
     // Handle filter by price
-    const handleFilter = (price) => {
-        setPriceFilter(price);
-    };
+    // const handleFilter = (price) => {
+    //     setPriceFilter(price);
+    // };
 
     // Handle select by price
-    const handleSelectChange = (e) => {
-        setPrice(e.target.value);
-        handleFilter(e.target.value);
-    };
+    const handleSelectChange = (event) => {
+        const selectedValue = event.target.value;
+      
+        // Filter products based on the selected price range
+        let filteredProducts;
+      
+        if (selectedValue === 'all') {
+          filteredProducts = originalProducts;
+        } else if (selectedValue === '10-20') {
+          filteredProducts = filterProductsByPrice(10, 20);
+        } else if (selectedValue === '20-30') {
+          filteredProducts = filterProductsByPrice(20, 30);
+        } else if (selectedValue === '30-40') {
+          filteredProducts = filterProductsByPrice(30, 40);
+        } else if (selectedValue === 'higher price') {
+          filteredProducts = filterProductsByPrice(100, 1000); // Adjust the upper limit accordingly
+        } else if (selectedValue === 'lower price') {
+          filteredProducts = filterProductsByPrice(0, 10); // Adjust the lower limit accordingly
+        }
+      
+        // Update the state with the filtered products
+        setProducts(filteredProducts);
+      };
+      
+      const filterProductsByPrice = (minPrice, maxPrice) => {
+        return originalProducts.filter(
+          (product) => product.price >= minPrice && product.price <= maxPrice
+        );
+      };
 
     // Read more
-    const displayedProducts = filteredProducts.slice(0, displayCount);
+    const displayedProducts = products.slice(0, displayCount);
     const handleViewMore = () => {
         setDisplayCount(displayCount + 4);
     };
@@ -159,12 +189,14 @@ const ProductHome = () => {
                 <h1 className="htxthead"><span className="spennofStyle"></span>Product</h1>
                 <div className="categoryBoxfiler">
                     <form>
-                        <select className="filter_priceProduct" value={price} onChange={handleSelectChange}>
-                            <option value="">Price</option>
-                            <option value="10">$10</option>
-                            <option value="20">$20</option>
-                            <option value="30">$30</option>
-                        </select>
+                    <select className="filter_priceProduct" value={selectedPriceRange} onChange={handleSelectChange}>
+                        <option value="all">All</option>
+                        <option value="10-20">10 to 20</option>
+                        <option value="20-30">20 to 30</option>
+                        <option value="30-40">30 to 40</option>
+                        <option value="higher price">Higher Price</option>
+                        <option value="lower price">Lower Price</option>
+                    </select>
                     </form>
                     <box-icon name='filter'></box-icon>
                 </div>
