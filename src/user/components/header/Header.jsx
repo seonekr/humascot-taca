c8b5fdd51862ed864e2ae3a84df18358100a20d3
seonekr <zaemuevln42gmail.com>
2023-11-15 11:07:48 +0700

    active menu

--- src/user/components/header/Header.jsx
+++ src/user/components/header/Header.jsx
@@ -14,25 +14,25 @@ const Header = () => {
     return (
         <>
             <section id="header">
-                <div className="navbar">
-                    <div className="headWithBox">
-                        <ul className="headMenu">
-                            <li><Link to="/"><img src={Logo1} alt="Logo" /></Link></li>
-                            <div className="boxLiMenu">
-                                <li><Link to="/" className="linkLi active">Home</Link></li>
-                                <li><Link to="/product_search" className="linkLi">Shop</Link></li>
-                                <li><Link to="/order" className="linkLi">Order</Link></li>
-                                <li><Link to="/chatuser" className="linkLi">Contact</Link></li>
-                                <li><Link to="/login/" className="linkLi">Login</Link></li>
-                            </div>
-                        </ul>
-                        <ul className="ulHead_box">
-                            <li><Link to="/product_search/"><FaMagnifyingGlass className="head_colorr" /></Link></li>
-                            <li><Link to="/cart"><FaCartShopping className="head_colorr" /></Link></li>
-                            <li><Link to="/account"><FaRegUser className="head_colorr" /></Link></li>
-                        </ul>
-                    </div>
+            <div className="navbar">
+            <div className="headWithBox">
+                <ul className="headMenu">
+                <li><Link to="/"><img src={Logo1} alt="Logo" /></Link></li>
+                <div className="boxLiMenu">
+                    <li><Link to="/" className={`linkLi ${location.pathname === '/' ? 'active' : ''}`}>Home</Link></li>
+                    <li><Link to="/product_search" className={`linkLi ${location.pathname === '/product_search' ? 'active' : ''}`}>Shop</Link></li>
+                    <li><Link to="/order" className={`linkLi ${location.pathname === '/order' ? 'active' : ''}`}>Order</Link></li>
+                    <li><Link to="/chatuser" className={`linkLi ${location.pathname === '/chatuser' ? 'active' : ''}`}>Contact</Link></li>
+                    <li><Link to="/login/" className={`linkLi ${location.pathname === '/login/' ? 'active' : ''}`}>Login</Link></li>
                 </div>
+                </ul>
+                <ul className="ulHead_box">
+                <li><Link to="/product_search/"><FaMagnifyingGlass className="head_colorr" /></Link></li>
+                <li><Link to="/cart"><FaCartShopping className="head_colorr" /></Link></li>
+                <li><Link to="/account"><FaRegUser className="head_colorr" /></Link></li>
+                </ul>
+            </div>
+            </div>
             </section>
         </>
     )
