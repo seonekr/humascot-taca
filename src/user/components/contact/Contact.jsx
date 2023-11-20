5d077ec2c93b516fa24897114f711318501abd74
seonekr <oudonephengkhamlar@gmail.com>
2023-11-20 11:09:41 +0700

    Update Contact.jsx

--- src/user/components/contact/Contact.jsx
+++ src/user/components/contact/Contact.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
-import './Contact.css'
-import Header from '../header/Header';
-import Menu from '../menu/Menu';
+import React, { useEffect, useState } from "react";
+import "./Contact.css";
+import Header from "../header/Header";
+import Menu from "../menu/Menu";
+import { useActionData, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Contact = () => {
+  // For authenticate user
+  const [userAccount, setUserAccount] = useState("");
+  const [userDetail, setUserDetail] = useState([]);
+  const token = localStorage.getItem("token");
+
+  const navitage = useNavigate();
+
+  // For authen users
+  useEffect(() => {
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: import.meta.env.VITE_API + "/authen",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        if (response.data.Status === "Success") {
+          setUserAccount(response.data.decoded.id);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // For get user by id
+  useEffect(() => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "http://localhost:5000/getCustomer/" + userAccount,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        setUserDetail(response.data.Result);
+        console.log(JSON.stringify(userDetail));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <Header />
@@ -15,9 +69,8 @@ const Contact = () => {
         </div>
       </div>
       <Menu />
-
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
