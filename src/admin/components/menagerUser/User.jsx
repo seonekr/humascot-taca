37c4bc952c31f5540f3c35a2ebde4423c8cd708a
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2023-11-21 16:17:22 +0700

    Update to users

--- src/admin/components/menagerUser/User.jsx
+++ src/admin/components/menagerUser/User.jsx
@@ -3,8 +3,29 @@ import AdminMenu from '../adminMenu/AdminMenu';
 import { FaAngleLeft } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { AiOutlineDelete } from 'react-icons/ai';
+import { useState } from 'react';
 
 const User = () => {
+
+     // Delete product
+    const [deleteUserId, setDeleteUserId] = useState(null);
+    const [isConfirmationPopupOpen, setConfirmationPopupOpen] = useState(false);
+
+    const openConfirmationPopup = (userID) => {
+        setDeleteUserId(userID);
+      setConfirmationPopupOpen(true);
+    };
+
+    const closeConfirmationPopup = () => {
+        setDeleteUserId(null);
+      setConfirmationPopupOpen(false);
+    };
+
+    const deleteUsers = () => {
+        console.log('Successful')
+        
+        closeConfirmationPopup();
+    };
     return(
         <>
             <AdminMenu/>
@@ -27,7 +48,7 @@ const User = () => {
                        <div>Password: </div>
                        <div>Password: </div>
                        <div className='del' >
-                           <AiOutlineDelete/>
+                           <AiOutlineDelete  onClick={() => openConfirmationPopup(user.userID)}/>
                        </div>
                     </div>
                     <div className="img">
@@ -36,17 +57,19 @@ const User = () => {
                 </div>
             </section>
 
-            {/* <div className="confirmation-popup">
+            {isConfirmationPopupOpen && (
+              <div className="confirmation-popup">
                 <p>Are you sure you want to delete?</p>
                 <div className="btn_ok_on">
-                    <button  className="btn_yes">
-                      Yes
-                    </button>
-                    <button  className="btn_on">
-                      No
-                    </button>
+                  <button onClick={deleteUsers} className="btn_yes">
+                    Yes
+                  </button>
+                  <button onClick={closeConfirmationPopup} className="btn_on">
+                    No
+                  </button>
                 </div>
-            </div> */}
+              </div>
+            )}
         </>
     )
 }
