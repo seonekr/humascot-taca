08ac4649311d310ea64ae65cfde8ab77bc975b42
seonekr <oudonephengkhamlar@gmail.com>
2023-11-21 16:03:13 +0700

    update

--- src/admin/components/menagerUser/User.jsx
+++ src/admin/components/menagerUser/User.jsx
@@ -1,54 +1,93 @@
-import './user.css';
-import AdminMenu from '../adminMenu/AdminMenu';
+import "./user.css";
+import AdminMenu from "../adminMenu/AdminMenu";
 import { FaAngleLeft } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import { AiOutlineDelete } from 'react-icons/ai';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { AiOutlineDelete } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import user from "../../../img/user.png";
 
 const User = () => {
-    return(
-        <>
-            <AdminMenu/>
-            <section id='user'>
-                <div className="back">
-                    <Link to="/users" className='link-back'>
-                        <FaAngleLeft id='icon_back_user'/>
-                        Back
-                    </Link>
-                    <div>
-                        User
-                    </div>
-                </div>
-                <div className="userInfo">
-                    <div className="info">
-                       <div>User ID: </div>
-                       <div>User Name: </div>
-                       <div>User Email: </div>
-                       <div>User Phone number: </div>
-                       <div>Password: </div>
-                       <div>Password: </div>
-                       <div className='del' >
-                           <AiOutlineDelete/>
-                       </div>
-                    </div>
-                    <div className="img">
-                       <img src='' alt="image" />
-                    </div>
-                </div>
-            </section>
+  const [userDetail, setUserDetail] = useState([]);
+  const [deleteUser, setDeleteUser] = useState(null);
+  const [isConfirmationPopupOpen, setConfirmationPopupOpen] = useState(false);
+  const { id } = useParams();
 
-            {/* <div className="confirmation-popup">
-                <p>Are you sure you want to delete?</p>
-                <div className="btn_ok_on">
-                    <button  className="btn_yes">
-                      Yes
-                    </button>
-                    <button  className="btn_on">
-                      No
-                    </button>
-                </div>
-            </div> */}
-        </>
-    )
-}
+  const navigate = useNavigate();
+
+  // For get user by id
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(import.meta.env.VITE_API + "/getCustomer/" + id, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.Status === "Success") {
+          setUserDetail(result.Result[0]);
+          console.log(userDetail);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  const UserDelete = (id) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:5000/deleteCustomer/" + id, requestOptions)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
+
+  return (
+    <>
+      <AdminMenu />
+      <section id="user">
+        <div className="back">
+          <Link to="/users" className="link-back">
+            <FaAngleLeft id="icon_back_user" />
+            Back
+          </Link>
+          <div>User</div>
+        </div>
+        <div className="userInfo">
+          <div className="info">
+            <div>User ID: {userDetail.id}</div>
+            <div>
+              User Name: {userDetail.fname} {userDetail.lname}
+            </div>
+            <div>User Email: {userDetail.email}</div>
+            <div>User Phone number: {userDetail.tel}</div>
+            <div>Password: ********</div>
+            <div className="del">
+              <AiOutlineDelete />
+            </div>
+          </div>
+          <div className="img">
+            {userDetail.profile_image ? (
+              <img src={userDetail.profile_image} alt="admin profile" />
+            ) : (
+              <img src={user} alt="admin profile" />
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default User;
