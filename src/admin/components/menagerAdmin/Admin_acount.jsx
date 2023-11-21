88804450269c1907658ad7281a511ed4bf35c4f9
seonekr <oudonephengkhamlar@gmail.com>
2023-11-21 11:06:03 +0700

    Update Admin_acount.jsx

--- src/admin/components/menagerAdmin/Admin_acount.jsx
+++ src/admin/components/menagerAdmin/Admin_acount.jsx
@@ -1,52 +1,45 @@
-import AdminMenu from '../adminMenu/AdminMenu';
+import AdminMenu from "../adminMenu/AdminMenu";
 import { FaAngleLeft } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import { MdOutlineEdit } from 'react-icons/md';
-import { AiOutlineDelete } from 'react-icons/ai';
-
+import { Link } from "react-router-dom";
+import { MdOutlineEdit } from "react-icons/md";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const Admin_acount = () => {
+  return (
+    <>
+      <AdminMenu />
+      <section id="user">
+        <div className="back">
+          <Link to="/admins/" className="link-back">
+            <FaAngleLeft />
+            Back
+          </Link>
+          <div>Your informations</div>
+        </div>
+        <div className="userInfo">
+          <div className="info">
+            <div>User ID: </div>
+            <div>User Name: </div>
+            <div>User Email: </div>
+            <div>User Phone number: </div>
+            <div>Password: </div>
+            <div>Password: </div>
+            <div className="del-update">
+              <div className="del">
+                <AiOutlineDelete />
+              </div>
+              <div className="update upd">
+                <MdOutlineEdit />
+              </div>
+            </div>
+          </div>
+          <div className="img">
+            <img src="" alt="image" />
+          </div>
+        </div>
+      </section>
 
-   
-
-    return(
-        <>
-            <AdminMenu/>
-            <section id='user'>
-                <div className="back">
-                    <Link to="/admins/" className='link-back'>
-                        <FaAngleLeft/>
-                        Back
-                    </Link>
-                    <div>
-                        Your informations
-                    </div>
-                </div>
-                <div className="userInfo">
-                    <div className="info">
-                        <div>User ID: </div>
-                        <div>User Name: </div>
-                        <div>User Email: </div>
-                        <div>User Phone number: </div>
-                        <div>Password: </div>
-                        <div>Password: </div>
-                        <div className='del-update'>
-                            <div className='del' >
-                                <AiOutlineDelete/>
-                            </div>
-                            <div className='update upd' >
-                                <MdOutlineEdit/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="img">
-                        <img src='' alt="image" />
-                    </div>
-                </div>
-                
-            </section>
-
-              {/* <div className="confirmation-popup">
+      {/* <div className="confirmation-popup">
                 <p>Are you sure you want to delete?</p>
                 <div className="btn_ok_on">
                   <button  className="btn_yes">
@@ -57,8 +50,8 @@ const Admin_acount = () => {
                   </button>
                 </div>
               </div> */}
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default Admin_acount;
