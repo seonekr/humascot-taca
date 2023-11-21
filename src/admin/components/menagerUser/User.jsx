960599e9bea203b4ab181669c64ccc3ff640b181
seonekr <145427113+seonekr@users.noreply.github.com>
2023-11-21 16:28:35 +0700

    Update

--- src/admin/components/menagerUser/User.jsx
+++ src/admin/components/menagerUser/User.jsx
@@ -1,41 +1,34 @@
 import "./user.css";
 import AdminMenu from "../adminMenu/AdminMenu";
 import { FaAngleLeft } from "react-icons/fa";
-import { Link } from 'react-router-dom';
 import { AiOutlineDelete } from 'react-icons/ai';
-import { useState } from 'react';
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { AiOutlineDelete } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import user from "../../../img/user.png";
-const User = () => {
-
-     // Delete product
-    const [deleteUserId, setDeleteUserId] = useState(null);
-    const [isConfirmationPopupOpen, setConfirmationPopupOpen] = useState(false);
-
-    const openConfirmationPopup = (userID) => {
-        setDeleteUserId(userID);
-      setConfirmationPopupOpen(true);
-    };
-
-    const closeConfirmationPopup = () => {
-        setDeleteUserId(null);
-      setConfirmationPopupOpen(false);
-    };
-
-    const deleteUsers = () => {
-        console.log('Successful')
-        
-        closeConfirmationPopup();
-    };
 
 const User = () => {
+  // Delete product
+  const [deleteUserId, setDeleteUserId] = useState(null);
+  const [isConfirmationPopupOpen, setConfirmationPopupOpen] = useState(false);
+
+  const openConfirmationPopup = (userID) => {
+    setDeleteUserId(userID);
+    setConfirmationPopupOpen(true);
+  };
+
+  const closeConfirmationPopup = () => {
+    setDeleteUserId(null);
+    setConfirmationPopupOpen(false);
+  };
+
+  const deleteUsers = () => {
+    console.log('Successful')
+
+    closeConfirmationPopup();
+  };
   const [userDetail, setUserDetail] = useState([]);
   const { id } = useParams();
-
   const navigate = useNavigate();
-
   // For get user by id
   useEffect(() => {
     var myHeaders = new Headers();
@@ -77,49 +70,49 @@ const User = () => {
 
   return (
     <>
-            <AdminMenu/>
-            <section id='user'>
-                <div className="back">
-                    <Link to="/users" className='link-back'>
-                        <FaAngleLeft id='icon_back_user'/>
-                        Back
-                    </Link>
-                    <div>
-                        User
-                    </div>
-                </div>
-                <div className="userInfo">
-                    <div className="info">
-                       <div>User ID: </div>
-                       <div>User Name: </div>
-                       <div>User Email: </div>
-                       <div>User Phone number: </div>
-                       <div>Password: </div>
-                       <div>Password: </div>
-                       <div className='del' >
-                           <AiOutlineDelete  onClick={() => openConfirmationPopup(user.userID)}/>
-                       </div>
-                    </div>
-                    <div className="img">
-                       <img src='' alt="image" />
-                    </div>
-                </div>
-            </section>
+      <AdminMenu />
+      <section id='user'>
+        <div className="back">
+          <Link to="/users" className='link-back'>
+            <FaAngleLeft id='icon_back_user' />
+            Back
+          </Link>
+          <div>
+            User
+          </div>
+        </div>
+        <div className="userInfo">
+          <div className="info">
+            <div>User ID: </div>
+            <div>User Name: </div>
+            <div>User Email: </div>
+            <div>User Phone number: </div>
+            <div>Password: </div>
+            <div>Password: </div>
+            <div className='del' >
+              <AiOutlineDelete onClick={() => openConfirmationPopup(user.userID)} />
+            </div>
+          </div>
+          <div className="img">
+            <img src='' alt="image" />
+          </div>
+        </div>
+      </section>
 
-            {isConfirmationPopupOpen && (
-              <div className="confirmation-popup">
-                <p>Are you sure you want to delete?</p>
-                <div className="btn_ok_on">
-                  <button onClick={deleteUsers} className="btn_yes">
-                    Yes
-                  </button>
-                  <button onClick={closeConfirmationPopup} className="btn_on">
-                    No
-                  </button>
-                </div>
-              </div>
-            )}
-        </>
+      {isConfirmationPopupOpen && (
+        <div className="confirmation-popup">
+          <p>Are you sure you want to delete?</p>
+          <div className="btn_ok_on">
+            <button onClick={deleteUsers} className="btn_yes">
+              Yes
+            </button>
+            <button onClick={closeConfirmationPopup} className="btn_on">
+              No
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
